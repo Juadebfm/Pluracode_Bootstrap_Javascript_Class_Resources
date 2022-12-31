1ae6ec3897719862fe83656c5bb87675241379e2
Julius Adebowale <juadebgabriel@gmail.com>
2022-12-31 12:06:29 -0600

    Javascript_

---
+++ javascript-challenges/jschallenge.js
@@ -0,0 +1,13 @@
+const myArray = [10, 20, 30, 40];
+myArray.push(50);
+myArray.push(60);
+myArray.push(60);
+myArray.push(60);
+
+console.log(myArray);
+
+let reverse = myArray.reverse();
+
+console.log(reverse);
+
+console.log(myArray.length, myArray);
